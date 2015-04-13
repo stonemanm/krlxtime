6223e848e2a3bfee08fed546907027bbd5bd72d3
Michael Stoneman <azureillusions@eduroam-171-201.dyn.carleton.edu>
2015-04-13 12:18:13 -0500

    Wrote out a very basic conception of what the program will be.

---
+++ krlxtime.java
@@ -0,0 +1,33 @@
+/**
+ * Displays the current class period at Carleton College. It's hardly the most
+ * sophisticated program, but it'll look nice on my desktop.
+ * 
+ * Last modified 2015-04-13
+ * @author Michael Stoneman
+ * @version 0.0.0? I guess?
+ */
+
+public class krlxtime {
+	
+
+
+	/**
+	 * Let's see. What do I actually want for the output?
+	 * In the circle, I want the name of the current period. Something along
+	 * the lines of "5a" will fit well, but "2/3c" could be iffier. We'll need
+	 * to write it as " 5a " or something.
+	 * 
+	 * ( 5a ) ends in n minutes.
+	 *        (k minutes until 6a.)
+	 *   
+	 * If there's not a current period (passing time, or within 90 minutes of
+	 * the next class), it could display time until next thing.
+	 * 
+	 * ( 10 ) minutes until 6a.
+	 * 
+	 * When the day is over, go to -- mode.
+	 */
+	public static void main(String[] args) {
+
+	}
+}
